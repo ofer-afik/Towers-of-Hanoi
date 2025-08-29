5cc37860d2ba9d2434e236a4c14b9b9f9e9341de
Ofer Afik <ofer.afik123@gmail.com>
2025-08-29 14:12:42 +0300

    Fix color problem take 2

--- script.js
+++ script.js
@@ -35,8 +35,10 @@ const moveHeader = document.getElementById("moves");
 function game(event) {
     disks.forEach(function (disk) {
         disk.remove();
-        disk.querySelector("button").style.color = "#000000";
     })
+    document.getElementById("but1").style.color = "#000000";
+    document.getElementById("but2").style.color = "#000000";
+    document.getElementById("but3").style.color = "#000000";
     towerDisks.tower1 = [];
     towerDisks.tower2 = [];
     towerDisks.tower3 = [];
